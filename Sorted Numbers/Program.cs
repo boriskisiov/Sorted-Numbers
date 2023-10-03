fd4788d76a8c1068b6cf340a4d5c6e9685717d3c
user <user@DESKTOP-G5S2LD7>
2023-10-03 19:49:23 +0300

    Add project files.

---
+++ Sorted Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿double num1 = double.Parse(Console.ReadLine());
+double num2 = double.Parse(Console.ReadLine());
+double num3  = double.Parse(Console.ReadLine());
+
+if (num1 > num2 && num2 > num3)
+{
+    Console.WriteLine("Decending");
+}
+else if (num1 < num2 && num2 < num3)
+{
+    Console.WriteLine("Asending");
+}
+else
+    Console.WriteLine("Not Sorted");
